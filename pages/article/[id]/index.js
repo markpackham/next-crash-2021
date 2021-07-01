8fcd1de12c73c4a49e65df5029d67b26c44cccf2
mark <markpackham1@gmail.com>
2021-07-01 15:25:15 +0100

    retrieve specific article by id, https://youtu.be/mTz0GXj8NN0?t=2573
    template literals wasn't working so used workaround

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,10 @@
+import { useRouter } from "next/router";
+
 const article = () => {
-  return <div>This is an article</div>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <div>This is article {id}</div>;
 };
 
 export default article;
